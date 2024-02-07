8e62a61b1863fe8275d652ff012c2e27c5ba80ba
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-07 17:54:32 -0300

    primeiro exercicio

---
+++ exercicio-um/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Concluido o primeiro <span>Exercicio</span></h1>
+    <p>Tesntando HTML</p>
+    </>
+  )
+}
+
+export default App
