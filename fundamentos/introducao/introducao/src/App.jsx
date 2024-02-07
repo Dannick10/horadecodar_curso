a247357d4f7b413fb4db9c784f3c56315f308ead
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-07 18:32:52 -0300

    Introdução aos fundamentos e componentes

---
+++ fundamentos/introducao/introducao/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+//Components
+import PrimeiroComponente from './components/primeirocomponente'
+//styles / css
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <PrimeiroComponente/>
+    </>
+  )
+}
+
+export default App
---
+++ fundamentos/introducao/introducao/src/components/PrimeiroComponente.jsx
@@ -0,0 +1,11 @@
+const PrimeiroComponente = () =>{
+
+    return(
+        <div>
+            <h1>Meu primeiro Componente</h1>
+        </div>
+    )
+
+}
+
+export default PrimeiroComponente
