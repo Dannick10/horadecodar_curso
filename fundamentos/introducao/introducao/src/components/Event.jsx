a7f2ee4d4a107dceaeeb6235001a57bbbb86f797
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-08 14:41:52 -0300

    funcoes no proprio evento

--- fundamentos/introducao/introducao/src/components/Event.jsx
+++ fundamentos/introducao/introducao/src/components/Event.jsx
@@ -12,9 +12,18 @@ const Event = () =>{
                 <button onClick={handleMyEvent}>Clique aqui!</button>
             </div>
 
-            <div>
-                <EventDois/>
-            </div>
+                <div>
+                    <button onClick={()=>{ console.log('clicou tmb')}}>Clique aqui tambem</button>
+                </div>
+
+               {/* <div>
+                    <button onClick={()=>{
+                        if(true){
+                            console.log('isso n deveria existir')
+                        }
+                    }}>Clique aqui pfv</button>
+                </div> */}
+           
         </div>
     )
 }
