0a49335fceddb3f4d16c53b6c1f28fc408ee07c8
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-08 16:46:34 -0300

    entrando no modulo avançado de react

---
+++ avancadoreact/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h3>Avançado REact</h3>
+
+      <div className='spide-home'>
+        <span>SPIDE</span>
+        <span>MAN</span>
+        <img src="/spideman.png" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
