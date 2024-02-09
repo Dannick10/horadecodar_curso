b03a9a642ecdb0a2bc4c70bcada3df7d320f53f7
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-09 20:49:00 -0300

    usando key e index

--- avancadoreact/src/components/ListRender.jsx
+++ avancadoreact/src/components/ListRender.jsx
@@ -4,11 +4,26 @@ const ListRender = () =>{
     const p = ['daniel','fabricio','paulo','ricardo']
     const [list] = useState(p)
 
+    const [users] = useState([
+        {id: 1, name:'Daniel', age: 24},
+        {id: 2, name: 'Mycaela', age: 24}
+    ])
+
     return(
         <div>
             <ul>
-                {list.map((item) => (
+                {list.map((item,i) => (
                     <li>{item}</li>
+                ))}
+            </ul>
+
+            <ul>
+                {users.map((user) =>(
+                    <li>
+                        key={user.id} |
+                        {user.name} |
+                        {user.age}
+                    </li>
                 ))}
             </ul>
 
