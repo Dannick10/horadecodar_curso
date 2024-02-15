e389ca3c9e37dd933f5d944c5ab01481db57d91a
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-15 13:24:17 -0300

    Css em componentes

--- css-react/src/App.jsx
+++ css-react/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+//components
+import Mycomponent from './components/Mycomponent'
+
+function App() {
+ 
   return (
     <>
       <div className="app">
     <h1>React com css</h1>
+
+    <Mycomponent/>
+    <p >Esse paragrafo e do app.js</p>
       </div>
     </>
   )
---
+++ css-react/src/components/Mycomponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Mycomponent.css'
+
+function Mycomponent() {
+  return (
+    <div>
+        <h1>CSS GLOBAL</h1>
+        <p className='p-Mycomponent'>AQUI</p>
+    </div>
+  )
+}
+
+export default Mycomponent
