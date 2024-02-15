1b0e33d0e665c1b1b65ac2013ed8a5778f99b602
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-15 13:45:37 -0300

    mudando classes dinamicas

--- css-react/src/App.jsx
+++ css-react/src/App.jsx
@@ -9,6 +9,7 @@ import Mycomponent from './components/Mycomponent'
 function App() {
 
   const x = false
+  const [redtitle,setredtitle] = useState(true)
 const [name] = useState('DANIEL')
  
   return (
@@ -26,6 +27,10 @@ const [name] = useState('DANIEL')
           <div className="test" style={x?{backgroundColor: 'black'}:{backgroundColor: 'purple'}}>AAAAAAAA</div>
 
           <div style={name=='DANIEL'?{color: 'red'}:{color: 'blue'}}>Daniel</div>
+
+          <h2 className={redtitle?'red-title':'title'}> Este TITULO TEM CLASSE DINAMICA</h2>
+          <button onClick={()=>setredtitle(true)}>Mudar cor</button>
+
       </div>
     </>
   )
