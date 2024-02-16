dc8721da92e9fec6aa1c0ece2c132494c209bd66
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-16 14:20:35 -0300

    envio de forms

--- formulario/src/components/Myforms.jsx
+++ formulario/src/components/Myforms.jsx
@@ -10,13 +10,21 @@ const MyForms = () =>{
     const handleName = (e)=>{
       setName(e.target.value)
     }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        console.log('enviando o form')
+        console.log(name,mall)
+    }
+
     console.log(mall)
     console.log(name)
 
     return(
         <div>
+            {/*Envio de form*/ }
                 {/*Criaçao de form*/ }
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="name">nome</label>
                         <input type="text" name="name" placeholder="digite seu nome" onChange={handleName}/>
@@ -25,6 +33,7 @@ const MyForms = () =>{
                 {/*label envolvendo input*/}
                 <label htmlFor="email">
                     <span>E-mall</span>
+                    {/*Simplificação*/}
                     <input type="email" name="email" placeholder='digite o seu email' onChange={(e)=>{setMall(e.target.value)}} />
                 </label>
                     <input type="submit" value="Enviar" />
