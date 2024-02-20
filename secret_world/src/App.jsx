6440d07311c10acebbb1cd866653e4ebbfb98e66
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-20 16:47:39 -0300

    fazendo components e preparando ambiente

--- secret_world/src/App.jsx
+++ secret_world/src/App.jsx
@@ -1,14 +1,36 @@
-
+//styles
 import './App.css'
 
+
+//react
+import { useCallback, useEffect, useState } from 'react'
+
+//componetnts
 import StartScreen from './components/StartScreen'
+import Game from './components/Game'
+import Over from './components/Over'
+
+//words
+import { wordsList } from './data/world'
+
+const stages = [
+  {id: 1, name:'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'}
+]
 
 function App() {
 
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  console.log(words)
 
   return (
     <>
-     <StartScreen/>   
+     {gameStage === 'start' && <StartScreen/>}
+     {gameStage === 'game' && <Game/>}  
+     {gameStage === 'end' && <Over/>}  
      
      </>
   )
---
+++ secret_world/src/components/Over.jsx
@@ -0,0 +1,11 @@
+
+
+function Over() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Over
