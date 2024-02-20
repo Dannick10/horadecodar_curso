c8d7f1dc544e39f7d16d3658764e7a748c95775b
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-20 16:34:53 -0300

    projeto secret word

---
+++ secret_world/src/components/StartScreen.jsx
@@ -0,0 +1,11 @@
+function StartScreen() {
+  return (
+    <div>
+        <h1>Secrets worlds</h1>
+        <p>Clique no botao abaixo para jogar</p>
+        <button>Começar</button>
+    </div>
+  )
+}
+
+export default StartScreen
---
+++ secret_world/src/data/world.jsx
@@ -0,0 +1,5 @@
+export const wordsList = {
+    carro: ['moto','porta','capo','pneu','antena'],
+    fruta: ['Banana','maça','abacati','abacaxi'],
+    profissao: ['programador','']
+}
