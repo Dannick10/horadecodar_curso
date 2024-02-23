1230bc8c4466a7f73ca44df9b39f9432370b4df9
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-22 22:58:28 -0300

    css do jogo

--- secret_world/src/components/Game.jsx
+++ secret_world/src/components/Game.jsx
@@ -3,9 +3,29 @@
 function Game({verifyletter}) {
   return (
     <div>
-            <h1>Game</h1>
-            <button onClick={verifyletter}>Finalizar jogo</button>
-        </div>
+           <p className="points">
+            <span>Pontuação: 0</span>
+           </p>
+           <h1>Adivinhe a palavra:</h1>
+           <h3 className="tip">Dica sobrea a palavra: <span>dica</span></h3>
+           <div className="wordContainer">
+            <span className="letter">A</span>
+            <span className="blankSquare"></span>
+           </div>
+
+           <div className="letterContainer">
+              <p>Tenta adivinhar uma letra da palavra</p>
+              <form action="">
+                <input type="text" name="letter" maxLength={1} required/>
+              </form>
+              <button>JOGAR!</button>
+           </div>
+          <div className="wrongLetterContainer">
+            <p>Letras já utilizadas</p>
+            <span>a,</span>
+            <span>b,</span>
+          </div>
+     </div>
   )
 }
 
