0c6dd8288bd1a8331bd22642b748ed6b3a81e69c
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-24 16:00:19 -0300

    primeira requisição api

--- httprequest/src/App.jsx
+++ httprequest/src/App.jsx
@@ -1,12 +1,41 @@
 import './App.css'
 
+import {useState,useEffect} from 'react'
+
+const url = "http://localhost:3000/products"
+
 function App() {
+
+  const [produtos,setProdutos] = useState([])
+
+  useEffect(()=>{
+    
+    async function fetchData(){
+      const res = await fetch(url)
+      const data = await res.json()
+
+      setProdutos(data)
+    }
+
+    fetchData()
+  },[])
+
+  
+
   
 
   return (
     <>
       <div className="app">
         <h1>Lista de produtos</h1>
+        <div className="produtos">
+          {produtos.map((p)=>(
+            <ul className='pr'>
+            <li key={p.id}>Produtos:{p.name}</li>
+            <li>Preço: {p.price}</li>
+            </ul>
+          ))}
+        </div>
       </div>
     </>
   )
