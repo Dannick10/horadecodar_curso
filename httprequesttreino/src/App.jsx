ca6146be71337aa21ecd92c96aad8bc3e3c538a2
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-24 16:35:08 -0300

    treinando fetch

--- httprequesttreino/src/App.jsx
+++ httprequesttreino/src/App.jsx
@@ -1,13 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const url = "http://localhost:3000/filmes"
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [film, setFilme] = useState([])
+
+  useEffect(()=>{
+
+     async function fetchData(){
+      const res = await fetch(url)
+      const data = await res.json()
+
+        setFilme(data)
+    } 
+    fetchData()
+  },[])
+
+  console.log(film)
 
   return (
     <>
+    <header>
+      <span>MYFILMS</span>
+      <ul>
+        <li>Inicio</li>
+        <li>Categorias</li>
+      </ul>
+      <button>Cadastrar</button>
+    </header>
+
+    <main>
+      {film.map((f)=>(
+        <div className='film'>
+          <h1 key={f.id} className='title'>{f.title}</h1>
+          <div className="img"><img src="https://www.filmproposals.com/image-files/independent-film-companies.jpg" alt="" /></div>
+          <p className='description'>{f.description}</p>
+          <span className='category'>Categoria: {f.category}</span>
+          <p>Ano: {f.ano}</p>
+          <p>{f.minutes} minutos</p>
+        </div>
+      ))}
+    </main>
      
     </>
   )
