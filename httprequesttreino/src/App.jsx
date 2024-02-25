8dad338cdaf58f1f4171552018fbec819e02cdab
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-25 18:21:31 -0300

    testando

--- httprequesttreino/src/App.jsx
+++ httprequesttreino/src/App.jsx
@@ -6,8 +6,17 @@ const url = "http://localhost:3000/filmes"
 function App() {
 
   const [film, setFilme] = useState([])
+  
+  const [title,setTitle] = useState()
+  const [description, setDescription] = useState()
+  const [img,setImg] = useState()
+  const [category,setCategory] = useState()
+  const [minutes,setMinutes] = useState()
+  const [ano,setAno] = useState()
+  const [link,setLink] = useState()
 
   useEffect(()=>{
+
 
      async function fetchData(){
       const res = await fetch(url)
@@ -17,6 +26,27 @@ function App() {
     } 
     fetchData()
   },[])
+
+  const handleSubmit = async (e) => {
+
+    const addFilme = {
+      title,
+      img,
+      description,
+      category,
+      minutes,
+      ano,
+      link
+    }
+
+    const res = await fetch(url,{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(addFilme) 
+    })
+  }
 
   console.log(film)
 
@@ -32,14 +62,51 @@ function App() {
     </header>
 
     <main>
+      <div className="addFilms">
+        <form onSubmit={handleSubmit}>
+       
+            <div>
+              <label>title
+                 <input type="text" name='title' value={title} onChange={(e)=> setTitle(e.target.value)} />
+                 </label>
+              <label>Imagem
+                 <input type="text" name='img' value={img} onChange={(e)=> setImg(e.target.value)} />
+                 </label>
+              <label>description
+                 <input type="text" name='description' value={description} onChange={(e)=> setDescription(e.target.value)} />
+                 </label>
+            </div>
+
+            <div>
+              <label>category
+                <input type="text" name='category' value={category} onChange={(e)=> setCategory(e.target.value)} />
+                </label>
+              <label>minutes
+                <input type="text" name='minutes' value={minutes} onChange={(e)=> setMinutes(e.target.value)} />
+                </label>
+              <label>ano
+                <input type="text" name='ano' value={ano} onChange={(e)=> setAno(e.target.value)} />
+                </label>
+            </div>
+        
+
+          <label>link 
+            <input type="text" name='link' value={link} onChange={(e)=> setLink(e.target.value)} />
+            </label>
+
+            <input className='sub' type="submit" value="Criar filme" />
+        </form>
+      </div>
+
       {film.map((f)=>(
-        <div className='film'>
+        <div className='film' style={{background:`url(${f.img})`, backgroundSize: 'cover', backgroundColor: "rgba(0,0,0,0.8)", backgroundBlendMode:"color"}}>
           <h1 key={f.id} className='title'>{f.title}</h1>
-          <div className="img"><img src="https://www.filmproposals.com/image-files/independent-film-companies.jpg" alt="" /></div>
+          <div className="img"><img src={f.img} alt="" /></div>
           <p className='description'>{f.description}</p>
           <span className='category'>Categoria: {f.category}</span>
           <p>Ano: {f.ano}</p>
           <p>{f.minutes} minutos</p>
+          <a href={f.link}><button>Assistir</button></a>
         </div>
       ))}
     </main>
