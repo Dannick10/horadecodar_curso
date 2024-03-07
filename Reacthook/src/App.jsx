039718a0ba50c2ac7c0065e6cb4f1d608f922605
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-03-07 20:02:44 -0300

    react router

--- Reacthook/src/App.jsx
+++ Reacthook/src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
 
+// 1 config react browser
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+//pages
+import Home from './pages/Home'
+import About from './pages/About'
+
 function App() {
   
 
   return (
     <>
-      
+      <h1>React Router</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        </Routes>      
+      </BrowserRouter> 
     </>
   )
 }
