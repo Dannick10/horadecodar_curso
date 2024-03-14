3bec52e1b1e08122f79eb04f5343bd4bdd0a17d8
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-03-14 18:25:42 -0300

    consumindo context

--- apicontext/src/App.jsx
+++ apicontext/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { useContext } from 'react'
+import {CounterContext} from './Context/CounterContext'
 
 function App() {
 
+  const {counter} = useContext(CounterContext)
+
   return (
     <>
-     
+     <h1>HOME</h1>
+     <p>Valor do context {counter}</p>
     </>
   )
 }
--- apicontext/src/Context/CounterContext.jsx
+++ apicontext/src/Context/CounterContext.jsx
@@ -1,4 +1,16 @@
 // 1 - criar context
 import { createContext, useState } from "react";
 
-export default CounterContext = createContext()
+export const CounterContext = createContext()
+
+// provide
+
+export const CounterContextProvider = ({children}) =>{
+    const [counter,SetCounter] = useState(5)
+
+    return(
+        <CounterContext.Provider value={{counter, SetCounter}}>
+            {children}
+        </CounterContext.Provider>
+    )
+}
