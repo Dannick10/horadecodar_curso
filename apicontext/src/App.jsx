025357a710158ea4a0299f9cebcdb0a2a80c2315
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-03-15 16:31:37 -0300

    refatorando hook

--- apicontext/src/App.jsx
+++ apicontext/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
-import { useContext } from 'react'
-import {CounterContext} from './Context/CounterContext'
+//import { useContext } from 'react'
+//import {CounterContext} from './Context/CounterContext'
 import ChangeCounter from './Components/ChangeCounte'
+
+import { useCounterContext } from './hooks/useCounterContext'
 
 function App() {
 
-  const {counter} = useContext(CounterContext)
+  //const {counter} = useContext(CounterContext)
+  const {counter} = useCounterContext()
 
   return (
     <>
