bb5695c2afa9df147c761bd685f9547cbe919487
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-03-15 16:19:17 -0300

    provider

--- apicontexttreine/src/App.jsx
+++ apicontexttreine/src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
 
+import { useContext } from 'react'
+import { CounterContext } from './Context/CounterContext'
+
+
 function App() {
-
-
+  const {counter} = useContext(CounterContext)
+console.log(counter)
   return (
     <>
-    
+    <h1>Consumindo contexto</h1>
+    <p>valor do CounterContextProvider: {counter}</p>
     </>
   )
 }
--- apicontexttreine/src/Context/CounterContext.jsx
+++ apicontexttreine/src/Context/CounterContext.jsx
@@ -1,3 +1,13 @@
 import { createContext, useState } from "react";
 
-export default CounterContext = createContext()
+export const CounterContext = createContext()
+
+export const CounterContextProvider = ({children})=> {
+    const [counter,SetCounter] = useState(100)
+
+    return( 
+       <CounterContext.Provider value={{counter,SetCounter}}>
+        {children}
+       </CounterContext.Provider>
+    )
+}
