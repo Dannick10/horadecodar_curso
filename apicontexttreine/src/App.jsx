1477f426df57a4b1b553a29f5bcbfd01cacbaea1
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-03-21 17:24:45 -0300

    treinando reduce

--- apicontexttreine/src/App.jsx
+++ apicontexttreine/src/App.jsx
@@ -9,14 +9,21 @@ import ChangeCounter from './componete/ChangeCounter'
 import { useColorContext } from './hooks/useColorContext'
 
 function App() {
-  const {color} = useColorContext()
+  const {color,dispatch} = useColorContext()
   //const {counter} = useContext(CounterContext)
   const {counter} = useCounterContext()
+
+  const SetchangeColor = (color) =>{
+    dispatch({type:color})
+  }
+
 console.log(counter)
   return (
     <>
     <h1 style={{color: color}}>Consumindo contexto</h1>
     <ChangeCounter/>
+    <button onClick={()=>SetchangeColor("RED")}>MUDARCOR</button>
+    <button onClick={()=>SetchangeColor("GREEN")}>a</button>
     <p>valor do CounterContextProvider: {counter}</p>
     </>
   )
--- apicontexttreine/src/Context/ChangeColorContext.jsx
+++ apicontexttreine/src/Context/ChangeColorContext.jsx
@@ -4,6 +4,14 @@ export const ChangeColorContext = createContext()
 
 export const titleColorReduce = (state,action) => {
 
+    switch(action.type){
+        case "RED":
+            return { ...state, color: "red"}
+        case "GREEN":
+            return { ...state, color: "green"}
+        default: 
+        return state
+    }
 }
 
 export const ChangeColorContextProvider = ({children}) => {
@@ -11,7 +19,7 @@ export const ChangeColorContextProvider = ({children}) => {
     const [state,dispatch] = useReducer(titleColorReduce, { color: "purple"})
         console.log(state)
     return(
-        <ChangeColorContext.Provider value={{...state}}>
+        <ChangeColorContext.Provider value={{...state,dispatch}}>
             {children}
         </ChangeColorContext.Provider>
     )
