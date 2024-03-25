b824358a0f4fea39eb1d706b2a72970404c03a9a
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-03-25 14:56:27 -0300

    firebase config

---
+++ frontblog/firebase/config.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxTXqUU073VVV7Ghy9JXR4FLkF_gc9bCs",
+  authDomain: "frontblog-cf6cc.firebaseapp.com",
+  projectId: "frontblog-cf6cc",
+  storageBucket: "frontblog-cf6cc.appspot.com",
+  messagingSenderId: "583032431241",
+  appId: "1:583032431241:web:43680bb5f6ead7fc8e9c58"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db }
