072b11e61c0339d7678513680766189061631b11
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-04-12 13:04:07 -0300

    resgatando no banco

--- frontblog/src/pages/search/Search.jsx
+++ frontblog/src/pages/search/Search.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+import styles from './Search.module.css'
 
 import { useFetchDocuments  } from '../../hooks/useFetchDocuments'
 import { useQuery  } from '../../hooks/useQuery'
+import PostDetails from '../../components/PostDetails'
 
 const Search = () => {
 
     const query = useQuery()
     const search = query.get('q')
 
+    const {documents: posts} = useFetchDocuments('posts',search)
+
   return (
-    <div>
+    <div className={styles.search}>
         <h1>Search</h1>
-        <p>{search}</p>
+      <div>
+        {posts && posts.length === 0 && (<p>Post não encontrado</p>)}
+        <Link to="" className='btn btn-dark'>Voltar</Link>
+        {posts && posts.map((post)=>(<PostDetails key={post.id} post={post}/>))}
+      </div>
     </div>
   )
 }
