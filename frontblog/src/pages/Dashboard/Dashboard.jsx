634cddfca90c0dfaf80f50dabbac4b7d06d870d4
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-04-15 16:33:37 -0300

    organizando posts na dashboard

--- frontblog/src/pages/Dashboard/Dashboard.jsx
+++ frontblog/src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import style from './Dashboard.module.css'
+
+import { Link } from 'react-router-dom'
+
+import { useAuthValue } from '../../context/AuthContext'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
 const Dashboard = () => {
+
+  const { user } = useAuthValue()
+  const uid = user.uid
+
+  const {documents: posts, loading } = useFetchDocuments('posts', null, uid)
+
+  //posts do usuario
   return (
-    <h1>Dashboard</h1>
+  <>
+    <h2>Dashboard</h2>
+
+    <p>Gerencie os seus posts</p>
+   {posts && posts.length === 0 ? (
+   <div className={StyleSheet.noposts}>
+      <p>Não foram encontrado posts</p>
+      <Link to='/posts/create' className='btn'>Crie primeiro posts</Link>
+   </div>):
+   (<div>
+    
+   </div>)}
+
+    {posts && posts.map((post)=>(<>
+      {post.title}
+    </>))}
+
+  </>
   )
 }
 
