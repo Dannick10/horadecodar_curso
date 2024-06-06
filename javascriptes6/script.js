7ceabf9d75d8c5991e4047156cd4d5cd610e5d6a
unknown <danmaggot2@gmail.com>
2024-06-06 16:23:10 -0300

    javascript:Es6 let const

---
+++ javascriptes6/script.js
@@ -0,0 +1,39 @@
+var x = 10
+var y = 15
+
+if( y > 10) {
+    var x = 5
+    console.log(x)
+}
+
+console.log(x)
+
+let a = 10
+let b = 15
+
+if(b > 10) {
+    let b= 5
+    console.log(b)
+}
+
+console.log(b)
+
+let i = 20
+console.log(i)
+
+for(let i =0; i <5; i++){
+    console.log(i)
+}
+
+console.log(i)
+
+function logName() {
+    const name = 'Daniel'
+    console.log(name)
+}
+
+const name = 'a'
+
+logName()
+
+console.log(name)
