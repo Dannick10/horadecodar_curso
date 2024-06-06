9a93991b1e894089c2f94f81ec0b74fae5e29458
unknown <danmaggot2@gmail.com>
2024-06-06 17:22:11 -0300

    spread

--- javascriptes6/script.js
+++ javascriptes6/script.js
@@ -162,3 +162,22 @@ const productDetails = {
 const {name: productName, price, category, color} = productDetails
 
 console.log(`nome do produto ${productName}, preço do produto é ${price}, pertece a categoria ${category} e é da cor ${color}`)
+
+const a1 = [1, 2, 3]
+const a2 = [4 ,5, 6]
+
+const a3 = [...a1,...a2]
+
+console.log(a3)
+
+const a4 = [0, ...a1, 4]
+
+console.log(a4)
+
+const carName = {name: 'gol'}
+const carBrand = {barnd: 'vw'}
+const carinfo = {barnd: '1000', price: 49000}
+
+const car = {...carName,...carBrand,...carinfo}
+
+console.log(car)
